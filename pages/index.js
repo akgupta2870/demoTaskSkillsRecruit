0c3734b3af9bbaa344f300a0647fcfce040eff85
akgupta2870 <82209087+akgupta2870@users.noreply.github.com>
2023-06-16 19:13:29 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,303 @@
+import React from "react";
+
+const Home = () => (
+  <div className="text-sans">
+    <>
+      <div className="bg-indigo-800 flex h-screen flex-col ">
+        <div className="bg-slate-400 m-2">
+          <div className="flex  bg-slate-300 rounded-lg m-2 p-2  justify-between items-center ">
+            <h1 className="text-xl text-indigo-700 font-bold ">
+              Pruchase Request Header Details
+            </h1>
+            <div className=" ">
+              <button
+                type="button"
+                class="focus:outline-none text-white mx-2 bg-red-800 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+              >
+                Draft
+              </button>
+              <button
+                type="button"
+                class="focus:outline-none text-white bg-red-500 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+          <form className="flex flex-col m-2 p-2 rounded justify-between bg-slate-400">
+            <div class="grid gap-2 my-6 mx-4 md:grid-cols-6">
+              <div>
+                <label
+                  for="first_name"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  First name
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="last_name"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Doe"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="company"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Company
+                </label>
+                <input
+                  type="text"
+                  id="company"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Flowbite"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="phone"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Phone number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="123-45-678"
+                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="website"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Website URL
+                </label>
+                <input
+                  type="url"
+                  id="website"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="flowbite.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="visitors"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Unique visitors (per month)
+                </label>
+                <input
+                  type="number"
+                  id="visitors"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  required
+                />
+              </div>
+            </div>
+            <div class="grid gap-2 mb-6 mx-4 md:grid-cols-6">
+              <div>
+                <label
+                  for="first_name"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  First name
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="last_name"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Doe"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="company"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Company
+                </label>
+                <input
+                  type="text"
+                  id="company"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Flowbite"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="phone"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Phone number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="123-45-678"
+                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="website"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Website URL
+                </label>
+                <input
+                  type="url"
+                  id="website"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="flowbite.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="visitors"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Unique visitors (per month)
+                </label>
+                <input
+                  type="number"
+                  id="visitors"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  required
+                />
+              </div>
+            </div>
+            <div class="mb-6 mx-4">
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <input
+                multiple
+                type="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john.doe@company.com"
+                required
+              />
+            </div>
+          </form>
+        </div>
+
+        <div className="flex flex-col bg-slate-400 m-2">
+          <div className="flex  bg-slate-300 rounded-lg m-2 p-2  justify-between items-center ">
+            <h1 className="text-xl text-indigo-700 font-bold ">
+              Pruchase Request Line Header Details
+            </h1>
+            <div className=" ">
+              <button
+                type="button"
+                class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                Add Entry
+              </button>
+            </div>
+          </div>
+          <table class="table-auto">
+            <thead>
+              <tr>
+                <th>Entry No </th>
+                <th>Items No </th>
+                <th>Item </th>
+                <th>Description </th>
+                <th>Type </th>
+                <th>division </th>
+                <th>Quantity </th>
+                <th>Price </th>
+                <th>Total Price </th>
+                <th>Deliver </th>
+                <th>Charge Amount </th>
+                <th>Deliver Amount </th>
+                <th>Changes </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>The Sliding Mr. Bones</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+                <td>Malcolm Lockyer</td>
+              </tr>
+              <tr>
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+                <td>Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+                <td>Witchy Woman</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  </div>
+);
+
+export default Home;
